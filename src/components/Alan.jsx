a723970c5204e5564a639f314f2fba2932988b16
yashmangal05 <103406113+yashmangal05@users.noreply.github.com>
2023-02-19 15:16:07 +0530

    Adding Logout and Login features with Voice Assistant

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import { fetchToken } from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
 
 const useAlan = () => {
@@ -16,6 +17,13 @@ const useAlan = () => {
                     } else {
                         setMode('dark');
                     }
+                } else if (command === 'login') {
+                    fetchToken();
+                } else if (command === 'logout') {
+
+                    localStorage.clear();
+
+                    window.location.href = '/';
                 }
             },
         });
